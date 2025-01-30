c1ac8b46567880871c3f117b09d473cfa04e4039
Suhani Sahu <sahusuhani14@gmail.com>
2025-01-30 18:53:29 +0530

    added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from 'tailwindcss';
+//import './src/app/globals.css'
 
 const config: Config = {
     darkMode: ["class"],
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./src/**/*.{js,ts,jsx,tsx}",
+           "./app/**/*.{js,ts,jsx,tsx}",
+            "./pages/**/*.{js,ts,jsx,tsx}",
+            "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
   	extend: {
   		borderRadius: {
